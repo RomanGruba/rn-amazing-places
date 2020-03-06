0b22543e7649efd2a76d6bfccfd6c3dae8508c77
Roman Gruba <roman.gruba@gmail.com>
2020-03-06 22:03:31 +0200

    NewPlace

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
+import HeaderButton from "../components/HeaderButton";
 
 const PlacesListScreen = props => {
   return (
@@ -7,6 +10,25 @@ const PlacesListScreen = props => {
       <Text>PlacesListScreen</Text>
     </View>
   );
+};
+
+PlacesListScreen.navigationOptions = navData => {
+  return {
+    headerTitle: "All places",
+    headerRight: () => {
+      return (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+          <Item
+            title="Add place"
+            iconName={Platform.OS === "android" ? "md-add" : "ios-add"}
+            onPress={() => {
+              navData.navigation.navigate("NewPlace");
+            }}
+          />
+        </HeaderButtons>
+      );
+    }
+  };
 };
 
 const styles = StyleSheet.create({});
