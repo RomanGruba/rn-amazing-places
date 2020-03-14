7697924b245286250585600e5391eff4ae837f88
Roman Gruba <roman.gruba@gmail.com>
2020-03-14 22:42:48 +0200

    insertPlace

--- store/placesActions.js
+++ store/placesActions.js
@@ -1,4 +1,5 @@
 import * as FileSystem from "expo-file-system";
+import { insertPlace } from "../helpers/db";
 
 export const ADD_PLACE = "ADD_PLACE";
 
@@ -12,10 +13,20 @@ export const addPlace = (title, image) => {
         from: image,
         to: newPath
       });
+      const dbResult = await insertPlace(
+        title,
+        newPath,
+        "Dummy address",
+        15.6,
+        12.3
+      );
+      console.log(dbResult);
+      dispatch({
+        type: ADD_PLACE,
+        placeData: { id: dbResult.insertId, title: title, image: newPath }
+      });
     } catch (err) {
       throw err;
     }
-
-    dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } });
   };
 };
